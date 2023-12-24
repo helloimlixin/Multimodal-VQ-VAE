3feeb99301d3101c742b58cf5ec9039f159bb214
Xin Li <helloimlixin@gmail.com>
2023-12-24 15:37:24 -0500

    Initial commit.

---
+++ utils.py
@@ -0,0 +1,19 @@
+#
+# Created on Sun Dec 24 2023
+#
+# Copyright (c) 2023 Xin Li
+# Email: helloimlixin@gmail.com
+# All rights reserved.
+#
+
+import os
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+"""Plotting Utilities"""
+def plot_results(results):
+    """Plot the results of the experiments.
+    Args:
+        results (torch.Tensor): the results of the experiments.
+    """
+    pass
